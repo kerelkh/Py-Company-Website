7ac7c7323bf7f55ea66aef3e8b878a60f49f87e6
kerelkh <kerelkh@gmail.com>
2024-09-09 13:01:12 +0700

    Change contact us form label and add variant info warning based on input

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -13,12 +13,20 @@ def handle_mail(email, message, topic):
 
 
 with st.form(key="email_forms"):
-    user_email = st.text_input("Your Email Address")
+    user_email = st.text_input("Your Email", placeholder="example@gmail.com")
     user_topic = st.selectbox(label="What topic do you want to discuss?", options=[item[0] for i, item in topics.iterrows()])
-    user_message = st.text_area("Your Message")
+    user_message = st.text_area("Your Message", placeholder="Hello, we would like to contact your company.\nPlease "
+                                                            "reply us as soon as possible")
     submit = st.form_submit_button("Submit")
     if submit:
         if user_email and user_topic and user_message:
             handle_mail(user_email, user_message, user_topic)
         else:
-            st.warning("Please input data needed first.")
+            if not user_email:
+                st.warning("Please input your Email")
+            elif not user_topic:
+                st.warning("Please input your Topic")
+            elif not user_message:
+                st.warning("Please input your Message")
+            else:
+                st.warning("Please input data needed first.")
